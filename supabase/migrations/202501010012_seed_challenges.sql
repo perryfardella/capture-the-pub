68ebdefcb3cf12da0d94989281b7271cc1d512c3
Perry Fardella <perry.fardella@live.com>
2026-01-08 22:33:53 +0800

    update seeded challenges

--- supabase/migrations/202501010012_seed_challenges.sql
+++ supabase/migrations/202501010012_seed_challenges.sql
@@ -5,14 +5,14 @@ select
   'pub' as type,
   id as pub_id,
   case 
-    when name = 'The Federal Hotel' then 'Gambling challenge'
-    when name = 'Fremantle Buffalo Club' then 'Pool challenge'
-    when name = 'The National Hotel' then 'Where`s Wally challenge'
-    when name = 'Gage Roads Freo Brewery' then 'Ring challenge'
-    when name = 'Bathers Beach House' then 'Swimming challenge'
-    when name = 'Archie Brothers Fremantle' then 'Dragon punch 9600 challenge'
-    when name = 'Mons O`Shea' then 'Split the G challenge'
-    when name = 'Calamity`s Rod Freo' then 'Pour your own drink challenge'
+    when name = 'The Federal Hotel' then 'Gambling challenge: One member of your team must win a bet on the TAB machine with 20 to 1 odds or higher. (ie. turn $1 into $20)'
+    when name = 'Fremantle Buffalo Club' then 'Pool challenge: One of your team members must beat a Buffalo Club member at a game of pool.'
+    when name = 'The National Hotel' then 'Where`s Wally challenge: Your team must find the building featured in the attached photo.'
+    when name = 'Gage Roads Freo Brewery' then 'Ring challenge: Two members of your team must play the ring game in the courtyard and get both rings on the hook.'
+    when name = 'Bathers Beach House' then 'Water challenge: One member of your team must fully submerge themselves in the ocean.'
+    when name = 'Archie Brothers Fremantle' then 'Dragon challenge: One member of your team must score over 9600 points on the Dragon Punch machine.'
+    when name = 'Mons O`Shea' then 'Split the G challenge: One member of your team must split the G on a pint of Guinness.'
+    when name = 'Calamity`s Rod Freo' then 'Pour your own challenge: One member of your team must convince the bartender to let them pour their own drink from behind the bar.'
   end as description
 from pubs
 where name in (
